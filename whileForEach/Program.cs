71a292f11b166fab439eb9c192400b26acfe5f3a
hakantopcu1989 <89578374+hakantopcu1989@users.noreply.github.com>
2023-06-17 14:33:01 +0300

    Add files via upload

---
+++ whileForEach/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace whileForeach
+{
+     class Program
+{
+     static void Main(string[] args)
+    {
+        // while
+        // 1den başlayarak consoledan girilen sayıya kadar sayıl dahil ortalama hesaplayıp
+        //console a yazdıran program
+        Console.Write("Lütfen Bir Sayı Giriniz: ");
+        int sayi=int.Parse(Console.ReadLine());
+        int sayac=1;
+        int toplam=0;
+        while (sayac<=sayi)
+        {
+            toplam+=sayac;
+            sayac++;
+        }
+
+        Console.WriteLine(toplam/sayi);
+
+        // a dan z ye kadar tüm harfleri console a yazdır
+
+        char character='a';
+        while (character<'z')
+        {
+            Console.Write(character);
+            character ++;
+        }
+
+        //
+
+        Console.Write("****Foreach******");
+        string[] arabalar={"BMV","Ford","Fiat","Opel"};
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+}
+}
